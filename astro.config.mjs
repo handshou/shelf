37c138f61abe978793854d7abd457e56f6e85faa
handshou <12982599+handshou@users.noreply.github.com>
2024-09-10 07:02:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+const tina = ({
+  directiveName = 'tina'
+} = {}) => ({
+  name: 'tina-cms',
+  hooks: {
+    'astro:config:setup': ({
+      addClientDirective,
+      opts
+    }) => {
+      addClientDirective({
+        name: directiveName,
+        entrypoint: './client-directives/tina.mjs'
+      });
+    }
+  }
+});
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://hansel.co',
+  integrations: [mdx({}), react(), tina()],
+  output: "server",
+  adapter: vercel()
+});
