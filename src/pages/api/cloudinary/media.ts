4d9353f427649b794ace1e4e8a6af5dfd584f432
hansel <handshou@users.noreply.github.com>
2024-09-18 12:06:47 +0800

    fix: would this work

--- src/pages/api/cloudinary/media.ts
+++ src/pages/api/cloudinary/media.ts
@@ -18,9 +18,10 @@ const handler = createMediaHandler({
             if (process.env.NODE_ENV == 'development') {
                 return true
             }
-            const user = await isAuthorized(req)
+            return true
+            //const user = await isAuthorized(req)
 
-            return user ? user.verified : false;
+            //return user ? user.verified : false;
         } catch (e) {
             console.error(e)
             return false
