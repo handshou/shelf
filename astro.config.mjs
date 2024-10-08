34fea6970b63ca0f2bd78100a76ecdd717f4fbf5
hansel <handshou@users.noreply.github.com>
2024-10-08 23:13:01 +0800

    fix: applyBasestyles

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
   },
   integrations: [mdx({}), react(), tina(), 
       tailwind({
-          applyBaseStyles: true,
+          applyBaseStyles: false,
       })
   ],
   output: "server",
