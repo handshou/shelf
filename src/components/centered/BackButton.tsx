99118a5d2b38c1ee22e96167556c8a4b6838fa9a
hansel <handshou@users.noreply.github.com>
2024-10-18 18:37:36 +0800

    chore: use lint

--- src/components/centered/BackButton.tsx
+++ src/components/centered/BackButton.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
 import '@/styles/globals.css'
-import { Button } from "@/components/ui/button"
-import { ArrowLeftIcon } from "@radix-ui/react-icons"
+import { Button } from '@/components/ui/button'
+import { ArrowLeftIcon } from '@radix-ui/react-icons'
 
 export default function BackButton() {
-  return (
-    <a href="javascript:history.go(-1)" className="text-gray-500 hover:text-gray-700">
-      <Button onclick="javascript:history.go(-1)"  className="fixed top-6 left-6 z-20 bg-orange-500 text-white 
-        hover:bg-orange-600 transition-colors duration-200 hidden lg:flex">
-        <ArrowLeftIcon className="h-4 w-4" />back
-      </Button>
-    </a>
-  )
+	return (
+		<a href="javascript:history.go(-1)" className="text-gray-500 hover:text-gray-700">
+			<Button
+				onclick="javascript:history.go(-1)"
+				className="fixed top-6 left-6 z-20 bg-orange-500 text-white 
+        hover:bg-orange-600 transition-colors duration-200 hidden lg:flex"
+			>
+				<ArrowLeftIcon className="h-4 w-4" />
+				back
+			</Button>
+		</a>
+	)
 }
--- src/components/centered/TopNavigation.tsx
+++ src/components/centered/TopNavigation.tsx
@@ -2,88 +2,90 @@ import React, { useState, useEffect, useRef } from 'react'
 import { useTina } from 'tinacms/dist/react'
 
 import '@/styles/globals.css'
-import { Button } from "@/components/ui/button"
-import { ArrowLeftIcon, ChevronLeftIcon } from "@radix-ui/react-icons"
+import { Button } from '@/components/ui/button'
+import { ArrowLeftIcon, ChevronLeftIcon } from '@radix-ui/react-icons'
 
 import { SITE_TITLE } from '../../config'
 
 import { breadcrumbPosition, isBottom, showNavigation, titlePosition } from '../../store/titleStore'
 
 export function TopNavigation(props) {
-  const { data } = useTina(props)
-  const { title: tinaTitle } = data.travels
+	const { data } = useTina(props)
+	const { title: tinaTitle } = data.travels
 
-  const [showTitle, setShowTitle] = useState(false)
-  const [lastScrollY, setLastScrollY] = useState(0)
-  const titleRef = useRef(null)
+	const [showTitle, setShowTitle] = useState(false)
+	const [lastScrollY, setLastScrollY] = useState(0)
+	const titleRef = useRef(null)
 
-  useEffect(() => {
-    const titleElement = document.querySelector('.travel-title')
-    const crumbElement = document.querySelector('.travel-crumb')
-    window?.addEventListener(
-      'scroll',
-      () => {
-        const titleRect = titleElement?.getBoundingClientRect()
-        const crumbRect = crumbElement?.getBoundingClientRect()
-        titlePosition.set(titleRect?.top + titleRect?.height || 0)
-        breadcrumbPosition.set(crumbRect?.height || 0)
-        if (window.scrollY > lastScrollY) {
-          showNavigation.set(false)
-        } else {
-          showNavigation.set(true)
-        }
-        setLastScrollY(window.scrollY)
-      },
-      { passive: true }
-    )
+	useEffect(() => {
+		const titleElement = document.querySelector('.travel-title')
+		const crumbElement = document.querySelector('.travel-crumb')
+		window?.addEventListener(
+			'scroll',
+			() => {
+				const titleRect = titleElement?.getBoundingClientRect()
+				const crumbRect = crumbElement?.getBoundingClientRect()
+				titlePosition.set(titleRect?.top + titleRect?.height || 0)
+				breadcrumbPosition.set(crumbRect?.height || 0)
+				if (window.scrollY > lastScrollY) {
+					showNavigation.set(false)
+				} else {
+					showNavigation.set(true)
+				}
+				setLastScrollY(window.scrollY)
+			},
+			{ passive: true }
+		)
 
-    return window?.removeEventListener('wheel', () => {
-      const titleRect = titleElement?.getBoundingClientRect()
-      const crumbRect = crumbElement?.getBoundingClientRect()
-      titlePosition.set(titleRect?.top + titleRect?.height || 0)
-      breadcrumbPosition.set(crumbRect?.height || 0)
-    })
-  }, [lastScrollY])
+		return window?.removeEventListener('wheel', () => {
+			const titleRect = titleElement?.getBoundingClientRect()
+			const crumbRect = crumbElement?.getBoundingClientRect()
+			titlePosition.set(titleRect?.top + titleRect?.height || 0)
+			breadcrumbPosition.set(crumbRect?.height || 0)
+		})
+	}, [lastScrollY])
 
-  useEffect(() => {
-    titlePosition.subscribe((pos) => {
-      if (pos - breadcrumbPosition.get() >= 0 || isBottom.get()) {
-        setShowTitle(false)
-      } else {
-        setShowTitle(true)
-      }
-    })
-  }, [])
+	useEffect(() => {
+		titlePosition.subscribe((pos) => {
+			if (pos - breadcrumbPosition.get() >= 0 || isBottom.get()) {
+				setShowTitle(false)
+			} else {
+				setShowTitle(true)
+			}
+		})
+	}, [])
 
-  return (
-    <div className="travel-crumb sticky top-0 z-10 bg-white h-20">
-      <nav className="text-sm pt-8 pl-8" aria-label="Breadcrumb">
-        {showTitle ? (
-          <h2 className="font-serif text-4xl font-bold">{tinaTitle}</h2>
-        ) : (
-          <ol className="list-none p-0 flex">
-            <li className="flex items-center">
-              <a href="/" className="text-gray-500 hover:text-gray-700">
-                {SITE_TITLE}
-              </a>
-            </li>
-            <li className="flex items-center">
-              <span className="mx-2"><ChevronLeftIcon /></span>
-              <a href="/travels" className="text-gray-500 hover:text-gray-700">
-                travels
-              </a>
-            </li>
-            {isBottom.get() && (
-              <>
-                <li className="flex items-center text-gray-900 font-bold">
-                  <span className="mx-2"><ChevronLeftIcon /></span>
-                  {tinaTitle.toLowerCase()}
-                </li>
-              </>
-            )}
-          </ol>
-        )}
-      </nav>
-    </div>
-  )
+	return (
+		<div className="travel-crumb sticky top-0 z-10 bg-white h-20">
+			<nav className="text-sm pt-8 pl-8" aria-label="Breadcrumb">
+				{showTitle ? (
+					<h2 className="font-serif text-4xl font-bold">{tinaTitle}</h2>
+				) : (
+					<ol className="list-none p-0 flex">
+						<li className="flex items-center">
+							<a href="/" className="text-gray-500 hover:text-gray-700">
+								{SITE_TITLE}
+							</a>
+						</li>
+						<li className="flex items-center">
+							<span className="mx-2">
+								<ChevronLeftIcon />
+							</span>
+							<a href="/travels" className="text-gray-500 hover:text-gray-700">
+								travels
+							</a>
+						</li>
+						{isBottom.get() && (
+							<li className="flex items-center text-gray-900 font-bold">
+								<span className="mx-2">
+									<ChevronLeftIcon />
+								</span>
+								{tinaTitle.toLowerCase()}
+							</li>
+						)}
+					</ol>
+				)}
+			</nav>
+		</div>
+	)
 }
