c86b319ce21ed369e1ed3493716aaa36c14f4ef7
hansel <handshou@users.noreply.github.com>
2024-10-29 16:42:25 +0800

    test: use dynamic config over text

--- src/pages/projects/projects.test.ts
+++ src/pages/projects/projects.test.ts
@@ -1,7 +1,8 @@
 import { expect, test } from '@playwright/test'
+import { SITE_TITLE } from '@/config'
 
 test('projects is visible', async ({ page }) => {
 	await page.goto('/projects')
-	await expect(page.getByRole('heading', { name: '🎈' })).toBeVisible()
+	await expect(page.getByRole('heading', { name: SITE_TITLE })).toBeVisible()
 	await expect(page.locator('#scroll')).toContainText('Astro Notion')
 })
--- src/pages/travels/travels.test.ts
+++ src/pages/travels/travels.test.ts
@@ -1,8 +1,9 @@
 import { expect, test } from '@playwright/test'
+import { SITE_TITLE } from '@/config'
 
 test('travels is visible', async ({ page }) => {
 	await page.goto('/travels')
-	await expect(page.getByRole('heading', { name: '🎈' })).toBeVisible()
+	await expect(page.getByRole('heading', { name: SITE_TITLE })).toBeVisible()
 	await expect(
 		page.getByRole('link', { name: 'hanoi travels/hanoi/DSC_3337_zprxhy' })
 	).toBeVisible()
