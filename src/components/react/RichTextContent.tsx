ae62bf50fb7034f79845572dcef4abb78c326dd4
hansel <handshou@users.noreply.github.com>
2024-11-22 22:41:04 +0800

    style: 404 page

--- src/components/react/RichTextContent.tsx
+++ src/components/react/RichTextContent.tsx
@@ -1,58 +1,58 @@
 import { TinaMarkdown } from 'tinacms/dist/rich-text'
 
-import { convertDMSToDecimal, validateGPSCoordinates } from '../../lib/map'
-import { Map as EmbedMap, type MapType } from '@rc/Tina/Map'
+import { convertDMSToDecimal, validateGPSCoordinates } from '@/lib/map'
 import { IFrame } from '@rc/Tina/IFrame'
+import { Map as EmbedMap, type MapType } from '@rc/Tina/Map'
 import { CldImage, CldVideo } from '@rc/Tina/cloudinary'
 import type { LinkProps } from '@rc/Tina/link'
 
 const components = {
-  a: (props: LinkProps) => (
-    <a href={props.url} target="_blank" rel="noopener noreferrer">
-      {props.children}
-    </a>
-  ),
+	a: (props: LinkProps) => (
+		<a href={props.url} target="_blank" rel="noopener noreferrer">
+			{props.children}
+		</a>
+	),
 
-  img: (props: LinkProps) => (
-    <a href={props.url} target="_blank" rel="noopener noreferrer">
-      <CldImage url={props.url} />
-    </a>
-  ),
+	img: (props: LinkProps) => (
+		<a href={props.url} target="_blank" rel="noopener noreferrer">
+			<CldImage url={props.url} />
+		</a>
+	),
 
-  Map: (props: { gpsCoordinates: string; mapType: MapType }) => {
-    let { gpsCoordinates = "30 19' 45.49N, 35 26' 34.86E", mapType } = props
-    if (typeof validateGPSCoordinates(gpsCoordinates) === 'string')
-      gpsCoordinates = "30 19' 45.49N, 35 26' 34.86E"
+	Map: (props: { gpsCoordinates: string; mapType: MapType }) => {
+		let { gpsCoordinates = "30 19' 45.49N, 35 26' 34.86E", mapType } = props
+		if (typeof validateGPSCoordinates(gpsCoordinates) === 'string')
+			gpsCoordinates = "30 19' 45.49N, 35 26' 34.86E"
 
-    return (
-      <>
-        <EmbedMap {...convertDMSToDecimal(gpsCoordinates)} mapType={mapType} />
-      </>
-    )
-  },
+		return (
+			<>
+				<EmbedMap {...convertDMSToDecimal(gpsCoordinates)} mapType={mapType} />
+			</>
+		)
+	},
 
-  IFrame: (props: {
-    embedCode: string
-  }) => {
-    return <IFrame embedCode={props.embedCode} />
-  },
+	IFrame: (props: {
+		embedCode: string
+	}) => {
+		return <IFrame embedCode={props.embedCode} />
+	},
 
-  Video: (props: {
-    videoUrl: string
-  }) => {
-    return (
-      <div className="flex justify-center items-center">
-        <div className="w-2/3 max-w-xs">
-          <CldVideo videoUrl={props.videoUrl} />
-        </div>
-      </div>
-    )
-  },
+	Video: (props: {
+		videoUrl: string
+	}) => {
+		return (
+			<div className="flex justify-center items-center">
+				<div className="w-2/3 max-w-xs">
+					<CldVideo videoUrl={props.videoUrl} />
+				</div>
+			</div>
+		)
+	},
 }
 
-export const RichTextContent = ({ content }) =>
-  <article
-    className="
+export const RichTextContent = ({ content }) => (
+	<article
+		className="
             prose 
             prose-img:rounded-lg 
             prose-base 
@@ -68,7 +68,8 @@ export const RichTextContent = ({ content }) =>
             prose-td:!border-none
             prose-td:!p-1
             "
-  >
-    {/* @ts-ignore component types must match TinaMarkdown component types*/}
-    < TinaMarkdown components={components} content={content} />
-  </article>
+	>
+		{/* @ts-ignore component types must match TinaMarkdown component types*/}
+		<TinaMarkdown components={components} content={content} />
+	</article>
+)
