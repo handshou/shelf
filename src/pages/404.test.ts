0185e8a1a0606eaa0811eb85347c91c0f4e59925
hansel <handshou@users.noreply.github.com>
2024-11-23 14:01:21 +0800

    test: 404

---
+++ src/pages/404.test.ts
@@ -0,0 +1,34 @@
+import { SITE_TITLE } from '@/config'
+import { expect, test } from '@playwright/test'
+
+test.describe('missing page', () => {
+	test('leads to 404', async ({ page }) => {
+		await page.goto('/missingpage')
+		await expect(page.getByRole('heading', { name: SITE_TITLE })).toBeVisible()
+		await expect(page.locator('#scroll')).toContainText('uh oh, page not found')
+	})
+
+	test('in travels subdirectory leads to 404', async ({ page }) => {
+		await page.goto('/travels/missingpage')
+		await expect(page.getByRole('heading', { name: SITE_TITLE })).toBeVisible()
+		await expect(page.locator('#scroll')).toContainText('uh oh, page not found')
+	})
+
+	test('in blogs subdirectory leads to 404', async ({ page }) => {
+		await page.goto('/blog/missingpage')
+		await expect(page.getByRole('heading', { name: SITE_TITLE })).toBeVisible()
+		await expect(page.locator('#scroll')).toContainText('uh oh, page not found')
+	})
+
+	test('in projects subdirectory leads to 404', async ({ page }) => {
+		await page.goto('/projects/missingpage')
+		await expect(page.getByRole('heading', { name: SITE_TITLE })).toBeVisible()
+		await expect(page.locator('#scroll')).toContainText('uh oh, page not found')
+	})
+
+	test('in invalid subdirectory leads to 404', async ({ page }) => {
+		await page.goto('/invalidsubdirectory/missingpage')
+		await expect(page.getByRole('heading', { name: SITE_TITLE })).toBeVisible()
+		await expect(page.locator('#scroll')).toContainText('uh oh, page not found')
+	})
+})
