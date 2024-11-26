ba88be9a581fabe8d185ee2f8ee605560cf277d3
hansel <handshou@users.noreply.github.com>
2024-11-26 18:35:14 +0800

    feat: redirect post if not published and not admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,6 @@
-import pkg from '@tinacms/auth'
-
 export async function onRequest(context, next) {
-  const { isUserAuthorized } = pkg
-  const api_key = process.env.TINA_PUBLIC_CLIENT_ID || ""
-  context.locals.host = context.request.headers?.get('x-forwarded-host')
-  context.locals.referer = context.request.headers?.get('referer')
+  context.locals.host = await context.request.headers?.get('x-forwarded-host')
+  context.locals.referer = await context.request.headers?.get('referer')
 
   return next()
 }
