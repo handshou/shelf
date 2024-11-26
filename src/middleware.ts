1ac7db7ce16aaa1df13e5026a0a83656cf1a9e9d
hansel <handshou@users.noreply.github.com>
2024-11-26 20:12:36 +0800

    chore: remove console log

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 export async function onRequest(context, next) {
-	console.log(context.request.headers)
 	context.locals.host = await context.request.headers?.get('host')
 	context.locals.referer = await context.request.headers?.get('referer')
 
