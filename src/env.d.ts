ed141ad546954ac2270265e3a00fbaab8c4f30ad
hansel <handshou@users.noreply.github.com>
2024-11-26 18:14:15 +0800

    chore: add splitters to consider url

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,5 +4,7 @@
 declare namespace App {
   interface Locals {
     token?: string,
+    referer?: string,
+    host?: string,
   }
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,8 @@ import pkg from '@tinacms/auth'
 export async function onRequest(context, next) {
   const { isUserAuthorized } = pkg
   const api_key = process.env.TINA_PUBLIC_CLIENT_ID || ""
-  context.locals.token = context.request.headers
+  context.locals.host = context.request.headers['x-forwarded-host']
+  context.locals.referer = context.request.headers?.referer
 
   return next()
 }
