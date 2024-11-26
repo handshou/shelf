4c972a43b2930ce20cba205dde6a1d4aefede979
hansel <handshou@users.noreply.github.com>
2024-11-26 18:18:23 +0800

    chore: improve header getter

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,8 @@ import pkg from '@tinacms/auth'
 export async function onRequest(context, next) {
   const { isUserAuthorized } = pkg
   const api_key = process.env.TINA_PUBLIC_CLIENT_ID || ""
-  context.locals.host = context.request.headers['x-forwarded-host']
-  context.locals.referer = context.request.headers?.referer
+  context.locals.host = context.request.headers?.get('x-forwarded-host')
+  context.locals.referer = context.request.headers?.get('referer')
 
   return next()
 }
