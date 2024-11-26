ebf6d67a60150a4c429ddda7a3fa4d282e2deaa4
hansel <handshou@users.noreply.github.com>
2024-11-26 17:26:38 +0800

    chore: put entire headers object into token locals

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import pkg from '@tinacms/auth'
 export async function onRequest(context, next) {
   const { isUserAuthorized } = pkg
   const api_key = process.env.TINA_PUBLIC_CLIENT_ID || ""
-  context.locals.token = context.request.headers?.authorization
+  context.locals.token = context.request.headers
 
   return next()
 }
