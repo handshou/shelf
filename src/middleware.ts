851e943eab963cc882f5a2112f5929c53a88bc74
hansel <handshou@users.noreply.github.com>
2024-11-26 15:20:23 +0800

    chore: log headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,8 @@ import pkg from '@tinacms/auth'
 export async function onRequest(context, next) {
   const { isUserAuthorized } = pkg
   const api_key = process.env.TINA_PUBLIC_CLIENT_ID || ""
-  const token = process.env.TINA_CONTENT_TOKEN || ""
-  const auth = await isUserAuthorized({ clientID: api_key, token: token })
-  context.auth = auth
-  console.log('auth', auth)
+  console.log(context.request.headers)
+  // const auth = await isUserAuthorized({ clientID: api_key, token: token })
 
   return next()
 }
