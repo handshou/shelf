a323d29e9b6021b3046a6a2a91dd445999078dbd
hansel <handshou@users.noreply.github.com>
2024-11-27 00:18:21 +0800

    test: projects
    test: projects

--- src/pages/projects/projects.test.ts
+++ src/pages/projects/projects.test.ts
@@ -1,8 +1,38 @@
-import { SITE_TITLE } from '@/config'
 import { expect, test } from '@playwright/test'
 
-test('projects is visible', async ({ page }) => {
-	await page.goto('/projects')
-	await expect(page.getByRole('heading', { name: SITE_TITLE })).toBeVisible()
-	await expect(page.locator('#scroll')).toContainText('Astro Notion')
+test.describe('projects', () => {
+  test.describe.configure({ mode: 'serial' })
+
+  test('is visible', async ({ page }) => {
+	  await page.goto('/')
+    await expect(page.getByRole('link', { name: '👨‍💻   projects' })).toBeVisible();
+  })
+
+  test('is clickable', async ({ page }) => {
+	  await page.goto('/')
+    await expect(page.getByRole('link', { name: '👨‍💻   projects' })).toBeVisible();
+    await page.getByRole('link', { name: '👨‍💻   projects' }).click();
+    await expect(page.getByRole('heading', { name: '🎈 hansel' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Astro Notion List of updates' })).toBeVisible();
+  })
+
+  test('is rendered', async ({ page }) => {
+	  await page.goto('/')
+    await page.getByRole('link', { name: '👨‍💻   projects' }).click();
+    await page.getByRole('link', { name: 'Astro Notion List of updates' }).click();
+    await expect(page.locator('div').filter({ hasText: 'back to 👨‍💻 projects' }).nth(3)).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Astro Notion' })).toBeVisible();
+    await expect(page.getByText('A long term project of designing and writing process.', { exact: true })).toBeVisible();
+    await expect(page.getByRole('heading', { name: '🎈 hansel' })).toBeVisible();
+  })
+
+  test('is backtrackable', async ({ page }) => {
+	  await page.goto('/')
+    await page.getByRole('link', { name: '👨‍💻   projects' }).click();
+    await page.getByRole('link', { name: 'Astro Notion List of updates' }).click();
+    await page.locator('div').filter({ hasText: 'back to 👨‍💻 projects' }).nth(3).click();
+    await expect(page.getByRole('link', { name: 'Astro Notion List of updates' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: '🎈 hansel' })).toBeVisible();
+  })
 })
+
