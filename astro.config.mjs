ec89e13382b175276acea3c3a3538a0152fa5037
hansel <handshou@users.noreply.github.com>
2024-11-27 07:22:12 +0800

    feat: speed up travels page load by defering image load

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,36 +7,37 @@ import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 
 const tina = ({ directiveName = 'tina' } = {}) => ({
-	name: 'tina-cms',
-	hooks: {
-		'astro:config:setup': ({ addClientDirective, opts }) => {
-			addClientDirective({
-				name: directiveName,
-				entrypoint: './client-directives/tina.mjs',
-			})
-		},
-	},
+  name: 'tina-cms',
+  hooks: {
+    'astro:config:setup': ({ addClientDirective, opts }) => {
+      addClientDirective({
+        name: directiveName,
+        entrypoint: './client-directives/tina.mjs',
+      })
+    },
+  },
 })
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hansel.co',
-	image: {
-		domains: ['res.cloudinary.com'],
-	},
-	integrations: [
-		mdx({}),
-		react(),
-		tina(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-	],
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true },
-	}),
-	experimental: {
-		contentLayer: true,
-	},
+  site: 'https://hansel.co',
+  image: {
+    domains: ['res.cloudinary.com'],
+  },
+  integrations: [
+    mdx({}),
+    react(),
+    tina(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+  experimental: {
+    contentLayer: true,
+    serverIslands: true,
+  },
 })
