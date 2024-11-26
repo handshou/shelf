1d6380c1c9ab1e2cc68b7d4941d9688c79a361ec
hansel <handshou@users.noreply.github.com>
2024-11-26 15:34:35 +0800

    chore: add auth header to locals

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,7 @@ import pkg from '@tinacms/auth'
 export async function onRequest(context, next) {
   const { isUserAuthorized } = pkg
   const api_key = process.env.TINA_PUBLIC_CLIENT_ID || ""
-  console.log(context.request.headers)
-  // const auth = await isUserAuthorized({ clientID: api_key, token: token })
+  context.locals.token = context.request.headers?.authorization
 
   return next()
 }
