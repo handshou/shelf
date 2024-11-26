73e1156edbef74d7bdc94b45d15691f3409a02ac
hansel <handshou@users.noreply.github.com>
2024-11-26 20:07:17 +0800

    fix: store host instead of x-forward-host header

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 export async function onRequest(context, next) {
-	context.locals.host = await context.request.headers?.get('x-forwarded-host')
+  console.log(context.request.headers)
+	context.locals.host = await context.request.headers?.get('host')
 	context.locals.referer = await context.request.headers?.get('referer')
 
 	return next()
