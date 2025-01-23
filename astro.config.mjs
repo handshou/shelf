f07ac32e42c4aa275c1d17ece37fcbe619960291
hansel <handshou@users.noreply.github.com>
2025-01-23 22:19:56 +0800

    chore: remove deprecated import
    The "@astrojs/vercel/serverless" import is deprecated and will be removed in a future release. Please import from "@astrojs/vercel" instead.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import mdx from '@astrojs/mdx'
 import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 import tailwind from '@astrojs/tailwind'
 
